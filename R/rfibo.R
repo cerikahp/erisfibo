859eb7221c5172ae22c8f0cb1abafd421ed1a6c2
Claudia Erika Hernandez Patino <cerikahp@gmail.com>
2019-10-02 18:05:41 -0500

    add function

---
+++ R/rfibo.R
@@ -0,0 +1,20 @@
+
+#' Recursive nth element
+#'
+#' @param n nth index
+#'
+#' @return nth fibonacci
+#' @export
+#'
+#' @examples
+#' rfibo()
+#'
+rfibo<-function(n=0){
+  if(n==0){
+    return(0)
+  }
+  if(n==1){
+    return(1)
+  }
+  return(rfibo(n-1)+rfibo(n-2))
+}
